0cb6919ab0c185896f6e957e420f20d4bd4fc69d
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-08-15 18:51:33 +0500

    update code legal delivery controller

--- dist/controllers/legalDelivery.controller.js
+++ dist/controllers/legalDelivery.controller.js
@@ -33,62 +33,46 @@ const search = (0, AsyncHandler_1.asyncHandler)(async (req, res) => {
 });
 exports.search = search;
 const searchInService = async (data) => {
-    if (!data) {
+    console.log("data>>>>>>>>>", data);
+    if (Object.keys(data).length === 0 && data.constructor === Object) {
         throw new ApiError_1.ApiError(http_status_codes_1.StatusCodes.BAD_REQUEST, "Search data is missing");
     }
     let dateTranformed = data.dateCreated ? data.dateCreated.split('/').join('-') : null;
     let populateData = ['clientId', 'serviceType', 'lTServiceType', 'standardServiceType', 'serviceFormCreatedBy', 'lastUpdatedBy'];
     console.log('Date Transformed: ', dateTranformed);
-    const serviceForms = await serviceForm_model_1.ServiceForm.find({
-        $or: [
-            {
-                inputDate: dateTranformed ? dateTranformed : null,
-            },
-            {
-                jobNo: data.jobNo ? data.jobNo : null
-            },
-            {
-                clientId: data.clientId ? data.clientId : null
-            },
-            {
-                serviceType: data.serviceType ? data.serviceType : null
-            },
-            {
-                caseNo: data.caseNo ? data.caseNo : null
-            },
-            {
-                fullName: data.fullName ? data.fullName : null
-            },
-            {
-                commercialDescription: data?.commercialDescription ? data?.commercialDescription : null,
-            },
-            {
-                zip: data?.zip ? data?.zip : null,
-            },
-            {
-                state: data?.state ? data?.state : null,
-            },
-            {
-                city: data?.city ? data?.city : null,
-            },
-            {
-                apt: data?.apt ? data?.apt : null,
-            },
-            {
-                address: data?.address ? data?.address : null,
-            },
-            {
-                businessName: data?.businessName ? data?.businessName : null,
-            },
-            {
-                otherLTDescription: data?.businessName ? data?.businessName : null,
-            }
-            // {
-            //     lTServiceType: data.lTServiceTypes ? data.lTServiceTypes.forEach((lt: string) => lt) : null 
-            // },
-        ]
-    }).populate(populateData);
-    if (serviceForms.length === 0 || !serviceForms) {
+    // Create a dynamic query object
+    let query = {};
+    if (dateTranformed)
+        query.inputDate = dateTranformed;
+    if (data.jobNo)
+        query.jobNo = data.jobNo;
+    if (data.clientId)
+        query.clientId = data.clientId;
+    if (data.serviceType)
+        query.serviceType = data.serviceType;
+    if (data.caseNo)
+        query.caseNo = data.caseNo;
+    if (data.fullName)
+        query.fullName = data.fullName;
+    if (data.commercialDescription)
+        query.commercialDescription = data.commercialDescription;
+    if (data.zip)
+        query.zip = data.zip;
+    if (data.state)
+        query.state = data.state;
+    if (data.city)
+        query.city = data.city;
+    if (data.apt)
+        query.apt = data.apt;
+    if (data.address)
+        query.address = data.address;
+    if (data.businessName)
+        query.businessName = data.businessName;
+    if (data.otherLTDescription)
+        query.otherLTDescription = data.otherLTDescription;
+    console.log("query>>>>>>>>>>>>>>>>>>>>>>>>>>>>", query);
+    const serviceForms = await serviceForm_model_1.ServiceForm.find(query).populate(populateData);
+    if (serviceForms.length === 0) {
         throw new ApiError_1.ApiError(http_status_codes_1.StatusCodes.NOT_FOUND, "Service form is not found");
     }
     return serviceForms;
@@ -98,29 +82,41 @@ const searchInResult = async (data) => {
     if (!data) {
         throw new ApiError_1.ApiError(http_status_codes_1.StatusCodes.BAD_REQUEST, "Search data is missing");
     }
-    let dateEnteredTransformed = data.dateEntered ? data.dateEntered.split('/').join('-') : null;
-    let dateServiceTransformed = data?.dateService ? data?.dateService.split("/").join("-") : null;
-    let dateFirstAttemptTransformed = data?.date1Attepmt ? data?.date1Attepmt.split("/").join("-") : null;
-    let dateSecondAttepmtTransformed = data?.date2Attepmt ? data?.date2Attepmt.split("/").join("-") : null;
-    let dateThirdAttepmtTransformed = data?.date3Attepmt ? data?.dateEntered?.split("/").join("-") : null;
-    let dateMailingTransformed = data?.dateMailing ? data?.dateMailing?.split("/").join("-") : null;
-    let populateData = ['resultOptions', 'serviceTypeOptions', 'substituteDeliveredTo'];
-    const resultForms = await resultForm_model_1.ResultForm.find({
-        $or: [
-            { queryInformationLTInputDate: dateEnteredTransformed ? dateEnteredTransformed : null },
-            { serviceResultDateOfService: dateServiceTransformed ? dateServiceTransformed : null },
-            { serviceResultFirstAttemptDate: dateFirstAttemptTransformed ? dateFirstAttemptTransformed : null },
-            { serviceResultSecondAttemptDate: dateSecondAttepmtTransformed ? dateSecondAttepmtTransformed : null },
-            { serviceResultThirdAttemptDate: dateThirdAttepmtTransformed ? dateThirdAttepmtTransformed : null },
-            { serviceResultDateOfMailing: dateMailingTransformed ? dateMailingTransformed : null },
-            { serviceResultResults: data?.resultOptions ? data?.resultOptions : null },
-            { serviceResultScvType: data?.serviceTypeOptions ? data?.serviceTypeOptions : null },
-            { substituteDeliveredTo: data?.substituteDeliveredTo ? data?.substituteDeliveredTo : null },
-            { serviceResultRecipient: data?.corpRecipient ? data?.corpRecipient : null },
-            { serviceResultRecipientTitle: data?.corpRecipientTitle ? data?.corpRecipientTitle : null },
-        ]
-    }).populate(populateData);
-    if (resultForms.length === 0 || !resultForms) {
+    // Transforming date fields
+    const dateEnteredTransformed = data.dateEntered ? data.dateEntered.split('/').join('-') : null;
+    const dateServiceTransformed = data.dateService ? data.dateService.split("/").join("-") : null;
+    const dateFirstAttemptTransformed = data.date1Attepmt ? data.date1Attepmt.split("/").join("-") : null;
+    const dateSecondAttemptTransformed = data.date2Attepmt ? data.date2Attepmt.split("/").join("-") : null;
+    const dateThirdAttemptTransformed = data.date3Attepmt ? data.date3Attepmt.split("/").join("-") : null;
+    const dateMailingTransformed = data.dateMailing ? data.dateMailing.split("/").join("-") : null;
+    // Dynamically building the query object
+    const query = {};
+    if (dateEnteredTransformed)
+        query.queryInformationLTInputDate = dateEnteredTransformed;
+    if (dateServiceTransformed)
+        query.serviceResultDateOfService = dateServiceTransformed;
+    if (dateFirstAttemptTransformed)
+        query.serviceResultFirstAttemptDate = dateFirstAttemptTransformed;
+    if (dateSecondAttemptTransformed)
+        query.serviceResultSecondAttemptDate = dateSecondAttemptTransformed;
+    if (dateThirdAttemptTransformed)
+        query.serviceResultThirdAttemptDate = dateThirdAttemptTransformed;
+    if (dateMailingTransformed)
+        query.serviceResultDateOfMailing = dateMailingTransformed;
+    if (data.serviceTypeOptions)
+        query.serviceResultScvType = data.serviceTypeOptions;
+    if (data.substituteDeliveredTo)
+        query.substituteDeliveredTo = data.substituteDeliveredTo;
+    if (data.corpRecipient)
+        query.serviceResultRecipient = data.corpRecipient;
+    if (data.corpRecipientTitle)
+        query.serviceResultRecipientTitle = data.corpRecipientTitle;
+    // Logging the query for debugging
+    console.log('Query Object:', query);
+    // Executing the query
+    const resultForms = await resultForm_model_1.ResultForm.find(query);
+    // Handling no results found
+    if (resultForms.length === 0) {
         throw new ApiError_1.ApiError(http_status_codes_1.StatusCodes.NOT_FOUND, "Result form is not found");
     }
     return resultForms;
@@ -130,44 +126,36 @@ const searchInStandard = async (data) => {
     if (!data) {
         throw new ApiError_1.ApiError(http_status_codes_1.StatusCodes.BAD_REQUEST, "Search data is missing");
     }
-    const serviceForms = await serviceForm_model_1.ServiceForm.find({
-        $or: [
-            {
-                oSSTDescription: data?.otherStdDescription ? data?.otherStdDescription : null
-            },
-            {
-                oSSTIndexNo: data?.indexNumber ? data?.indexNumber : null
-            },
-            {
-                sSDCourt: data?.court ? data?.court : null
-            },
-            {
-                cityServe: data?.city ? data?.city : null
-            },
-            {
-                sSDCountry: data?.country ? data?.country : null
-            },
-            {
-                sSDPlaintiff: data?.plaintiff ? data?.plaintiff : null
-            },
-            {
-                sSDDefendants: data?.defendant ? data?.defendant : null
-            },
-            {
-                firstNameServe: data?.fullName ? data?.fullName : null
-            },
-            {
-                addressServe: data?.address ? data?.address : null
-            },
-            {
-                aptServe: data?.apt ? data?.apt : null
-            },
-            {
-                zipServe: data?.zip ? data?.zip : null
-            },
-        ]
-    });
-    if (serviceForms.length === 0 || !serviceForms) {
+    // Dynamically building the query object
+    const query = {};
+    if (data.otherStdDescription)
+        query.oSSTDescription = data.otherStdDescription;
+    if (data.indexNumber)
+        query.oSSTIndexNo = data.indexNumber;
+    if (data.court)
+        query.sSDCourt = data.court;
+    if (data.city)
+        query.cityServe = data.city;
+    if (data.country)
+        query.sSDCountry = data.country;
+    if (data.plaintiff)
+        query.sSDPlaintiff = data.plaintiff;
+    if (data.defendant)
+        query.sSDDefendants = data.defendant;
+    if (data.fullName)
+        query.firstNameServe = data.fullName;
+    if (data.address)
+        query.addressServe = data.address;
+    if (data.apt)
+        query.aptServe = data.apt;
+    if (data.zip)
+        query.zipServe = data.zip;
+    // Logging the query for debugging
+    console.log('Query Object:', query);
+    // Executing the query
+    const serviceForms = await serviceForm_model_1.ServiceForm.find(query);
+    // Handling no results found
+    if (serviceForms.length === 0) {
         throw new ApiError_1.ApiError(http_status_codes_1.StatusCodes.NOT_FOUND, "Standard Service form is not found");
     }
     return serviceForms;
--- src/controllers/legalDelivery.controller.ts
+++ src/controllers/legalDelivery.controller.ts
@@ -46,8 +46,8 @@ const search = asyncHandler(async (req: Request, res: Response) => {
 });
 
 const searchInService = async (data: ISearchService) => {
-
-    if (!data) {
+    console.log("data>>>>>>>>>", data);
+    if (Object.keys(data).length === 0 && data.constructor === Object) {
         throw new ApiError(StatusCodes.BAD_REQUEST, "Search data is missing");
     }
 
@@ -56,154 +56,112 @@ const searchInService = async (data: ISearchService) => {
 
     console.log('Date Transformed: ', dateTranformed);
 
-    const serviceForms: IServiceFormDocument[] = await ServiceForm.find({
+    // Create a dynamic query object
+    let query: any = {};
 
-        $or: [
-            {
-                inputDate: dateTranformed ? dateTranformed : null,
-            },
-            {
-                jobNo: data.jobNo ? data.jobNo : null
-            },
-            {
-                clientId: data.clientId ? data.clientId : null
-            },
-            {
-                serviceType: data.serviceType ? data.serviceType : null
-            },
-            {
-                caseNo: data.caseNo ? data.caseNo : null
-            },
-            {
-                fullName: data.fullName ? data.fullName : null
+    if (dateTranformed) query.inputDate = dateTranformed;
+    if (data.jobNo) query.jobNo = data.jobNo;
+    if (data.clientId) query.clientId = data.clientId;
+    if (data.serviceType) query.serviceType = data.serviceType;
+    if (data.caseNo) query.caseNo = data.caseNo;
+    if (data.fullName) query.fullName = data.fullName;
+    if (data.commercialDescription) query.commercialDescription = data.commercialDescription;
+    if (data.zip) query.zip = data.zip;
+    if (data.state) query.state = data.state;
+    if (data.city) query.city = data.city;
+    if (data.apt) query.apt = data.apt;
+    if (data.address) query.address = data.address;
+    if (data.businessName) query.businessName = data.businessName;
+    if (data.otherLTDescription) query.otherLTDescription = data.otherLTDescription;
 
-            },
-            {
-                commercialDescription: data?.commercialDescription ? data?.commercialDescription : null,
-            },
-            {
-                zip: data?.zip ? data?.zip : null,
-            },
-            {
-                state: data?.state ? data?.state : null,
-            },
-            {
-                city: data?.city ? data?.city : null,
-            },
-            {
-                apt: data?.apt ? data?.apt : null,
-            }
-            ,
-            {
-                address: data?.address ? data?.address : null,
-            }
-            ,
-            {
-                businessName: data?.businessName ? data?.businessName : null,
-            },
-            {
-                otherLTDescription: data?.businessName ? data?.businessName : null,
-            }
-            
+    console.log("query>>>>>>>>>>>>>>>>>>>>>>>>>>>>",query)
+    const serviceForms: IServiceFormDocument[] = await ServiceForm.find(query).populate(populateData) as IServiceFormDocument[];
 
-            // {
-            //     lTServiceType: data.lTServiceTypes ? data.lTServiceTypes.forEach((lt: string) => lt) : null 
-            // },
-        ]
-    }).populate(populateData) as IServiceFormDocument[];
-
-
-    if (serviceForms.length === 0 || !serviceForms) {
-        throw new ApiError(StatusCodes.NOT_FOUND, "Service form is not found")
+    if (serviceForms.length === 0) {
+        throw new ApiError(StatusCodes.NOT_FOUND, "Service form is not found");
     }
 
     return serviceForms;
+};
 
-}
 
 const searchInResult = async (data: ISearchResult) => {
     if (!data) {
         throw new ApiError(StatusCodes.BAD_REQUEST, "Search data is missing");
     }
-    let dateEnteredTransformed=data.dateEntered ? data.dateEntered.split('/').join('-') : null;
-    let dateServiceTransformed=data?.dateService? data?.dateService.split("/").join("-") : null;
-    let dateFirstAttemptTransformed=data?.date1Attepmt? data?.date1Attepmt.split("/").join("-"):null;
-    let dateSecondAttepmtTransformed=data?.date2Attepmt? data?.date2Attepmt.split("/").join("-"):null;
-    let dateThirdAttepmtTransformed=data?.date3Attepmt? data?.dateEntered?.split("/").join("-"):null;
-    let dateMailingTransformed=data?.dateMailing ? data?.dateMailing?.split("/").join("-") :null;
-    let populateData = ['resultOptions', 'serviceTypeOptions', 'substituteDeliveredTo'];
-    const resultForms: IResultFormDocument[] = await ResultForm.find({
-        $or: [
-            {queryInformationLTInputDate:dateEnteredTransformed? dateEnteredTransformed : null},
-            {serviceResultDateOfService:dateServiceTransformed? dateServiceTransformed : null},
-            {serviceResultFirstAttemptDate:dateFirstAttemptTransformed? dateFirstAttemptTransformed : null},
-            {serviceResultSecondAttemptDate:dateSecondAttepmtTransformed? dateSecondAttepmtTransformed : null},
-            {serviceResultThirdAttemptDate:dateThirdAttepmtTransformed? dateThirdAttepmtTransformed : null},
-            {serviceResultDateOfMailing:dateMailingTransformed? dateMailingTransformed : null},
-            {serviceResultResults:data?.resultOptions ? data?.resultOptions : null},
-            {serviceResultScvType:data?.serviceTypeOptions ? data?.serviceTypeOptions : null},
-            {substituteDeliveredTo:data?.substituteDeliveredTo ? data?.substituteDeliveredTo : null},
-            {serviceResultRecipient:data?.corpRecipient ? data?.corpRecipient : null},
-            {serviceResultRecipientTitle:data?.corpRecipientTitle ? data?.corpRecipientTitle : null},         
-        ]
-    }).populate(populateData) as IResultFormDocument[];
-    
-    if (resultForms.length === 0 || !resultForms) {
-        throw new ApiError(StatusCodes.NOT_FOUND, "Result form is not found")
+
+    // Transforming date fields
+    const dateEnteredTransformed = data.dateEntered ? data.dateEntered.split('/').join('-') : null;
+    const dateServiceTransformed = data.dateService ? data.dateService.split("/").join("-") : null;
+    const dateFirstAttemptTransformed = data.date1Attepmt ? data.date1Attepmt.split("/").join("-") : null;
+    const dateSecondAttemptTransformed = data.date2Attepmt ? data.date2Attepmt.split("/").join("-") : null;
+    const dateThirdAttemptTransformed = data.date3Attepmt ? data.date3Attepmt.split("/").join("-") : null;
+    const dateMailingTransformed = data.dateMailing ? data.dateMailing.split("/").join("-") : null;
+
+    // Dynamically building the query object
+    const query: any = {};
+
+    if (dateEnteredTransformed) query.queryInformationLTInputDate = dateEnteredTransformed;
+    if (dateServiceTransformed) query.serviceResultDateOfService = dateServiceTransformed;
+    if (dateFirstAttemptTransformed) query.serviceResultFirstAttemptDate = dateFirstAttemptTransformed;
+    if (dateSecondAttemptTransformed) query.serviceResultSecondAttemptDate = dateSecondAttemptTransformed;
+    if (dateThirdAttemptTransformed) query.serviceResultThirdAttemptDate = dateThirdAttemptTransformed;
+    if (dateMailingTransformed) query.serviceResultDateOfMailing = dateMailingTransformed;
+    if (data.serviceTypeOptions) query.serviceResultScvType = data.serviceTypeOptions;
+    if (data.substituteDeliveredTo) query.substituteDeliveredTo = data.substituteDeliveredTo;
+    if (data.corpRecipient) query.serviceResultRecipient = data.corpRecipient;
+    if (data.corpRecipientTitle) query.serviceResultRecipientTitle = data.corpRecipientTitle;
+
+    // Logging the query for debugging
+    console.log('Query Object:', query);
+
+    // Executing the query
+    const resultForms: IResultFormDocument[] = await ResultForm.find(query) as IResultFormDocument[];
+
+    // Handling no results found
+    if (resultForms.length === 0) {
+        throw new ApiError(StatusCodes.NOT_FOUND, "Result form is not found");
     }
 
     return resultForms;
- }
+};
 
 
-const searchInStandard = async (data:ISearchStandard) => {
+
+ const searchInStandard = async (data: ISearchStandard) => {
     if (!data) {
         throw new ApiError(StatusCodes.BAD_REQUEST, "Search data is missing");
     }
 
-    const serviceForms: IServiceFormDocument[] = await ServiceForm.find({
-        $or: [
-            {
-                oSSTDescription: data?.otherStdDescription ? data?.otherStdDescription : null
-            },
-            {
-                oSSTIndexNo: data?.indexNumber ? data?.indexNumber : null
-            },
-            {
-                sSDCourt: data?.court ? data?.court : null
-            },
-            {
-                cityServe: data?.city ? data?.city : null
-            },
-            {
-                sSDCountry: data?.country ? data?.country : null
-            },
-            {
-                sSDPlaintiff: data?.plaintiff ? data?.plaintiff : null
-            },
-            {
-                sSDDefendants: data?.defendant ? data?.defendant : null
-            },
-            {
-                firstNameServe: data?.fullName ? data?.fullName : null
-            },
-            {
-                addressServe: data?.address ? data?.address : null
-            },
-            {
-                aptServe: data?.apt ? data?.apt : null
-            },
-            {
-                zipServe: data?.zip ? data?.zip : null
-            },
-        ]
-    });
+    // Dynamically building the query object
+    const query: any = {};
 
-    if (serviceForms.length === 0 || !serviceForms) {
-        throw new ApiError(StatusCodes.NOT_FOUND, "Standard Service form is not found")
+    if (data.otherStdDescription) query.oSSTDescription = data.otherStdDescription;
+    if (data.indexNumber) query.oSSTIndexNo = data.indexNumber;
+    if (data.court) query.sSDCourt = data.court;
+    if (data.city) query.cityServe = data.city;
+    if (data.country) query.sSDCountry = data.country;
+    if (data.plaintiff) query.sSDPlaintiff = data.plaintiff;
+    if (data.defendant) query.sSDDefendants = data.defendant;
+    if (data.fullName) query.firstNameServe = data.fullName;
+    if (data.address) query.addressServe = data.address;
+    if (data.apt) query.aptServe = data.apt;
+    if (data.zip) query.zipServe = data.zip;
+
+    // Logging the query for debugging
+    console.log('Query Object:', query);
+
+    // Executing the query
+    const serviceForms: IServiceFormDocument[] = await ServiceForm.find(query);
+
+    // Handling no results found
+    if (serviceForms.length === 0) {
+        throw new ApiError(StatusCodes.NOT_FOUND, "Standard Service form is not found");
     }
+
     return serviceForms;
- }
+};
+
 
 const checkltServiceDetail = (data: ISearchService) => {
     if (data.fullName || data.businessName || data.address || data.apt || data.city || data.zip || data.commercialDescription) {
